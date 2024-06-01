7c14c27df0914fec89846283ca1cd3e51b92dc0e
milton <miltondanielhch617@gmail.com>
2024-06-01 01:59:26 -0400

    agregando las tablas de migraciones en laravel

---
+++ app/Models/Course.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    use HasFactory;
+
+    const BORRADOR = 1, REVISION = 2, PUBLICADO = 3;
+}
